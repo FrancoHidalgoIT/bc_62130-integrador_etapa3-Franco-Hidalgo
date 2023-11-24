768078cd3d1c3678b13e1bf52924ecb1a4bbe2cd
Franco Hidalgo <francohidalgo2016@gmail.com>
2023-11-24 11:36:27 -0300

    retocando bd.productos

--- _ref/db.productos.js
+++ _ref/db.productos.js
@@ -1,94 +1,94 @@
 db.productos.insertMany (
-  [
-    {
-        "nombre": "IPhone 11 Pro Max",
-        "foto": "img/apple-iphone-11-pro-max.jpg",
-        "detalles": "Agregar al carro",
-        "precio": 250.5,
-         "stock": "5",
-        "marca": "Apple",
-        "categoria": "celulares",
-        "envio": false
-    },
-    {
-        "nombre": "IPhone 12",
-        "foto": "img/apple-iphone-12.jpg",
-        "detalles": "Agregar al carro",
-        "precio": 240.3,
-        "stock": "4",
-        "marca": "Apple",
-        "categoria": "celulares",
-        "envio": true
-    },
-    {
-        "nombre": "Auriculares Sony",
-        "foto": "img/auriculares-sony.jpg",
-        "detalles": "Agregar al carro",
-        "precio": 985,
-        "stock": 4,
-        "marca": "Sleek Cotton Chips",
-        "categoria": "Refined",
-        "envio": true
-    },
-    {
-        "nombre": "Camara Canon",
-        "detalles": "Agregar al carro",
-        "foto": "img/camara-canon.jpg",
-        "precio": 678,
-        "stock": 7,
-        "marca": "Rustic Soft Tuna",
-        "categoria": "Generic",
-        "envio": true
-    },
-    {
-          "nombre": "Samsung",
-        "detalles": "Agregar al carro",
-        "foto": "img/celular-samsung.jpg",
-        "precio": 1200,
-        "stock": 4,
-        "marca": "Samsung",
-        "categoria": "celulares",
-        "envio": true
-    },
-    {
-        "nombre": "Chromecast",
-        "precio": 332.22,
-        "stock": 4,
-        "marca": "Google",
-        "categoria": "Electronica",
-        "detalles": "Agregar al carro",
-        "foto": "img/chromecast-google.jpg",
-        "envio": true
-    },
-    {
-        "nombre": "DJI Mavic 2 pro",
-        "precio": 333.22,
-        "stock": 2,
-        "marca": "DJI",
-        "categoria": "Drones",
-        "detalles": "Agregar al carro",
-        "foto": "img/dji-mavic-2-pro.jpg",
-         "envio": false
-    },
-    {
-        "nombre": "Televisor 8K",
-        "precio": "2222",
-        "stock": "2",
-        "marca": "Sorny",
-        "categoria": "Electro",
-        "detalles": "Agregar al carro",
-        "foto": "img/img-televisor-1.webp",
-        "envio": true
-    },
-    {
-        "nombre": "Smartwatch",
-        "precio": "2222",
-        "stock": "2",
-        "marca": "Xiaomi",
-        "categoria": "Electro",
-        "detalles": "Agregar al carro",
-        "foto": "img/smartwatch.webp",
-        "envio": true
-    }
-  ]
+    [
+        {
+            "nombre": "IPhone 11 Pro Max",
+            "foto": "img/apple-iphone-11-pro-max.jpg",
+            "detalles": "Agregar al carro",
+            "precio": 250.5,
+            "stock": 5,
+            "marca": "Apple",
+            "categoria": "celulares",
+            "envio": false
+        },
+        {
+            "nombre": "IPhone 12",
+            "foto": "img/apple-iphone-12.jpg",
+            "detalles": "Agregar al carro",
+            "precio": 240.3,
+            "stock": 4,
+            "marca": "Apple",
+            "categoria": "celulares",
+            "envio": true
+        },
+        {
+            "nombre": "Auriculares Sony",
+            "foto": "img/auriculares-sony.jpg",
+            "detalles": "Agregar al carro",
+            "precio": 985,
+            "stock": 4,
+            "marca": "Sleek Cotton Chips",
+            "categoria": "Refined",
+            "envio": true
+        },
+        {
+            "nombre": "Camara Canon",
+            "detalles": "Agregar al carro",
+            "foto": "img/camara-canon.jpg",
+            "precio": 678,
+            "stock": 7,
+            "marca": "Rustic Soft Tuna",
+            "categoria": "Generic",
+            "envio": true
+        },
+        {
+            "nombre": "Samsung",
+            "detalles": "Agregar al carro",
+            "foto": "img/celular-samsung.jpg",
+            "precio": 1200,
+            "stock": 4,
+            "marca": "Samsung",
+            "categoria": "celulares",
+            "envio": true
+        },
+        {
+            "nombre": "Chromecast",
+            "precio": 332.22,
+            "stock": 4,
+            "marca": "Google",
+            "categoria": "Electronica",
+            "detalles": "Agregar al carro",
+            "foto": "img/chromecast-google.jpg",
+            "envio": true
+        },
+        {
+            "nombre": "DJI Mavic 2 pro",
+            "precio": 333.22,
+            "stock": 2,
+            "marca": "DJI",
+            "categoria": "Drones",
+            "detalles": "Agregar al carro",
+            "foto": "img/dji-mavic-2-pro.jpg",
+            "envio": false
+        },
+        {
+            "nombre": "Televisor 8K",
+            "precio": 2222,
+            "stock": 2,
+            "marca": "Sorny",
+            "categoria": "Electro",
+            "detalles": "Agregar al carro",
+            "foto": "img/img-televisor-1.webp",
+            "envio": true
+        },
+        {
+            "nombre": "Smartwatch",
+            "precio": 2222,
+            "stock": 2,
+            "marca": "Xiaomi",
+            "categoria": "Electro",
+            "detalles": "Agregar al carro",
+            "foto": "img/smartwatch.webp",
+            "envio": true
+        }
+    ]
 )
